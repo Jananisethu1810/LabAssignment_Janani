1c158585e6b8c4d6b604b115505c669e5c0cb41e
Janani S <60174771+Jananisethu1810@users.noreply.github.com>
2020-10-28 14:21:08 +0530

    Update PositiveString.java

--- Lab3_Assignment/src/PositiveString.java
+++ Lab3_Assignment/src/PositiveString.java
@@ -6,27 +6,27 @@
 import java.util.Scanner;
 public class PositiveString { 
 	public static boolean checkPositiveString(String str) {    // A method that accepts a String and checks if it is a positive string
-		int length=str.length();
-		for(int outerIndex=0;outerIndex<length;outerIndex++) {
-			char ch=str.charAt(outerIndex);
-			for(int innerIndex=outerIndex+1;innerIndex<length;innerIndex++) {
-				if(ch<str.charAt(innerIndex)) {
-					continue;
-				}
-				else
-					return false;
-			}
-		}
-		return true;
+            int length=str.length();
+	    for(int outerIndex=0;outerIndex<length;outerIndex++) {
+		char ch=str.charAt(outerIndex);
+		for(int innerIndex=outerIndex+1;innerIndex<length;innerIndex++) {
+		    if(ch<str.charAt(innerIndex)) {
+		       continue;
+		    }
+		    else
+		    return false;
+	        }
+	    }
+	    return true;
 	}
 
 	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-        System.out.println("Enter the input string : ");
-        String inputStr=scan.next();
-        boolean result=checkPositiveString(inputStr);
-        System.out.println(result);
-        scan.close();
+	    Scanner scan=new Scanner(System.in);
+            System.out.println("Enter the input string : ");
+            String inputStr=scan.next();
+            boolean result=checkPositiveString(inputStr);       //Function Call
+            System.out.println(result);
+            scan.close();
 	}
 
 }
