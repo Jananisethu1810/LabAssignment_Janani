f8e4328f0b470c268c19fcd01606f2d62bbe8f68
Janani S <60174771+Jananisethu1810@users.noreply.github.com>
2020-10-28 14:24:02 +0530

    Update ReplaceConsonants.java

--- Lab3_Assignment/src/ReplaceConsonants.java
+++ Lab3_Assignment/src/ReplaceConsonants.java
@@ -4,34 +4,33 @@
  *
  */
 import java.util.Scanner;
+
 public class ReplaceConsonants {
 	public static String alterString(String inputStr) { // To return the String replacing all the consonants with the next character.
-		String str="";
-		for(int itr=0;itr<inputStr.length();itr++) {
-        	char temp=inputStr.charAt(itr);
-        	if(temp=='a' || temp=='A' || temp=='e' || 
-        			temp=='E' || temp=='i' || temp=='I' || 
-        			temp=='o' || temp=='O' || temp=='u' || 
-        			temp=='U') {
-        		 str+=temp;
-        		
-        	}
-        	else {
-        		str+=(char)(temp+1);        //Replace consonants 
-        	}
-        }
-		return str;     
+		String str = "";
+		for (int itr = 0; itr < inputStr.length(); itr++) {
+			char temp = inputStr.charAt(itr);
+			if (temp == 'a' || temp == 'A' || temp == 'e' || temp == 'E' || temp == 'i' || temp == 'I' || temp == 'o'
+					|| temp == 'O' || temp == 'u' || temp == 'U') {                           // Checks whether the character is a vowel 
+				str += temp;
+
+			} 
+			else {
+				str += (char) (temp + 1); // Replace consonants
+			}
+		}
+		return str;
 	}
 
 	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-        System.out.println("Enter the input string :");
-        String inputStr=scan.next();
-        System.out.print("The output string is ");
-        String outputStr=alterString(inputStr);
-        System.out.println(outputStr);
-        scan.close();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the input string :");
+		String inputStr = scan.next();
+		System.out.print("The output string is ");
+		String outputStr = alterString(inputStr);       // Function Call
+		System.out.println(outputStr);
+		scan.close();
 	}
 
-	
 }
+
