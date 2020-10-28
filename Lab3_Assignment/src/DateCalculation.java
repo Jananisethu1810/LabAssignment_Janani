5fecde2a1f0f59f6289d56c90acd630360904cae
Janani S <60174771+Jananisethu1810@users.noreply.github.com>
2020-10-28 14:09:52 +0530

    Update DateCalculation.java

--- Lab3_Assignment/src/DateCalculation.java
+++ Lab3_Assignment/src/DateCalculation.java
@@ -1,31 +1,22 @@
-
 /**
  * Desc  - To print the duration in days, months and years with regards to current system date.
  * @author Janani S
  *
  */
-import java.text.ParseException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.Period;
-import java.util.*;
-public class DateCalculation {
+import java.time.*;
 
-	public static void main(String[] args) throws ParseException {
-		Scanner scan=new Scanner(System.in);
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		String inputString1 = scan.next();
-		LocalDate date1 = LocalDate.parse(inputString1, dtf);
-		LocalDate date2 = LocalDate.now();
-		System.out.println(dtf.format(date2));
-		Period period=Period.between(date1, date2);
-		long totalDays=period.getDays();
-		int totalMonths = period.getMonths();
-		int totalYears=period.getYears();
-		System.out.println ("Total Days: " + totalDays);
-		System.out.println ("Total Months: " + totalMonths);
-		System.out.println ("Total Years: " + totalYears);
-		scan.close();
+class DateCalculation {
+
+	public static void findDuration(LocalDate inputDate, LocalDate currDate) {
+		Period diff = Period.between(inputDate, currDate); // To calculate the difference between two dates in years, months, and days.
+		System.out.println("No.of days: " + diff.getDays());
+		System.out.println("No.of months: " + diff.getMonths());
+		System.out.println("No.of Years: " + diff.getYears());
 	}
 
+	public static void main(String[] args) {
+		LocalDate date1 = LocalDate.of(2018, 01, 10); // Start date
+		LocalDate date2 = LocalDate.now(); // End date
+		findDuration(date1, date2); // Function Call
+	}
 }
