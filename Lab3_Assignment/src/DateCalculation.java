709b18df4346f5eebbadde2d993a9e14a4e5e62c
Janani S <60174771+Jananisethu1810@users.noreply.github.com>
2020-10-28 14:17:13 +0530

    Update DateCalculation.java

--- Lab3_Assignment/src/DateCalculation.java
+++ Lab3_Assignment/src/DateCalculation.java
@@ -8,15 +8,14 @@ import java.time.*;
 class DateCalculation {
 
 	public static void findDuration(LocalDate inputDate, LocalDate currDate) {
-		Period diff = Period.between(inputDate, currDate); // To calculate the difference between two dates in years, months, and days.
-		System.out.println("No.of days: " + diff.getDays());
-		System.out.println("No.of months: " + diff.getMonths());
-		System.out.println("No.of Years: " + diff.getYears());
+	    Period diff = Period.between(inputDate, currDate); // To calculate the difference between two dates in years, months, and days.
+            System.out.println("No.of days: " + diff.getDays());
+	    System.out.println("No.of months: " + diff.getMonths());
+	    System.out.println("No.of Years: " + diff.getYears());
 	}
-
-	public static void main(String[] args) {
-		LocalDate date1 = LocalDate.of(2018, 01, 10); // Start date
-		LocalDate date2 = LocalDate.now(); // End date
-		findDuration(date1, date2); // Function Call
+        public static void main(String[] args) {
+	    LocalDate date1 = LocalDate.of(2018, 01, 10); // Start date
+	    LocalDate date2 = LocalDate.now(); // End date
+	    findDuration(date1, date2); // Function Call
 	}
 }
