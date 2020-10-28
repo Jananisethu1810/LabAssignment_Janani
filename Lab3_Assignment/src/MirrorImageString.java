9da3c0efae0bf157ab04ade1f5c1233db3573436
Janani S <60174771+Jananisethu1810@users.noreply.github.com>
2020-10-28 14:14:56 +0530

    Update MirrorImageString.java

--- Lab3_Assignment/src/MirrorImageString.java
+++ Lab3_Assignment/src/MirrorImageString.java
@@ -8,16 +8,16 @@ import java.util.Scanner;
 public class MirrorImageString {
     
 	public static String getImage(String str) {
-	        StringBuffer strBuffer=new StringBuffer(str);  //StringBuffer is used to make the string mutable. 
-                strBuffer.reverse();       // It reverses the string 
-                return strBuffer.toString();
+	    StringBuffer strBuffer=new StringBuffer(str);  //StringBuffer is used to make the string mutable. 
+            strBuffer.reverse();       // It reverses the string 
+            return strBuffer.toString();
 	}
 	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Enter the string:");
-		String str=scan.next();
-		String modifiedString=getImage(str);
-		System.out.println("The resultant string is "+str+"|"+modifiedString);
-                scan.close();
+	    Scanner scan=new Scanner(System.in);
+            System.out.println("Enter the string:");
+	    String str=scan.next();
+	    String modifiedString=getImage(str);        //Function Call
+	    System.out.println("The resultant string is "+str+"|"+modifiedString);
+            scan.close();
 	}
 }
